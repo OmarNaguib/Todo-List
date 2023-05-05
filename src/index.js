ca0ad111378e78fc7c487c13e494d5e308083027
Omar <55961349+OmarNaguib@users.noreply.github.com>
2023-05-05 23:06:17 +0300

    Remove console testers

--- src/index.js
+++ src/index.js
@@ -18,49 +18,6 @@ import * as userInterface from "./interface";
 import * as control from "./control";
 import "./style.css";
 
-// const main = "here";
-
-// console.log("projects", projects);
-
-// const input = {
-//   title: "title",
-//   description: "description",
-//   dueDate: "dueDate",
-//   priority: "priority",
-// };
-
-// const input2 = {
-//   title: "whatwhat",
-//   description: "description",
-//   dueDate: "dueDate",
-//   priority: "priority",
-// };
-
-// addProject("second");
-// projects[0].addTodo(input);
-// projects[0].addTodo(input2);
-// projects[0].addTodo(input);
-// console.log("projects[0]", projects[0]);
-
-// addProject("first");
-// projects[1].addTodo(input2);
-// projects[1].addTodo(input);
-// projects[1].addTodo(input);
-// // projects[0].editTodo(0, input2);
-// // console.log("projects[0]", projects[0]);
-// console.log(getAllTodos());
-// console.log(projects[0].getProjectTodos());
-// console.log(projects[1].getProjectTodos());
-
-// console.log(getProjectNames());
-// console.log(countProjects());
-
-// // removeProject(0);
-// // removeProject(1);
-// // removeProject(2);
-// console.log(countProjects());
-
-// beggining of real logic
 function refreshDisplay() {
   userInterface.reset();
   userInterface.displayTodos(
